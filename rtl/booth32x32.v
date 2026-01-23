d1081d62363381a5d1d11dad7b14ad5f65d59699
unlastingstar <a11790783629@qq.com>
2026-01-24 01:12:39 +0800

    modified booth32x32

--- rtl/booth32x32.v
+++ rtl/booth32x32.v
@@ -8,15 +8,17 @@ module booth32x32(Z, X, Y);
 
     wire [63:0] s [31:0];
 
-    assign s[31][32:0] = 33'b0;
+    assign s[31][31:0] = 32'b0;
 
     generate
         for(i=31;i>=1;i=i-1)
         begin : genboothlayer
-            booth_layer#(.n(64-i)) bl(s[i-1][(63-i):0], {{(33-i){X[31]}},X[30:0]}, Y[i], Y[i-1], s[i][(62-i):0]);            
+            booth_layer#(.n(63-i)) bl(s[i-1][(62-i):0], {{(32-i){X[31]}},X[30:0]}, Y[i], Y[i-1], s[i][(61-i):0]);            
         end
     endgenerate
 
-    booth_layer#(.n(64)) fbl(Z, {{33{X[31]}}, X[30:0]}, Y[0], 1'b0, s[0][62:0]);
+    booth_layer#(.n(63)) fbl(Z[62:0], {{32{X[31]}}, X[30:0]}, Y[0], 1'b0, s[0][61:0]);
+
+    buf b0(Z[63], Z[62]);
 
 endmodule
